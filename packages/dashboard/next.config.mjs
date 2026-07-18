636898616017421da0a80e59a243ca0a0e25aced
Shankar Warang <shankar@edgeware.community>
2026-07-18 20:41:25 +0530

    chore: fix metamask-sdk webpack warning

--- packages/dashboard/next.config.mjs
+++ packages/dashboard/next.config.mjs
@@ -10,6 +10,13 @@ const nextConfig = {
             },
         ];
     },
+    webpack: (config) => {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            '@react-native-async-storage/async-storage': false,
+        };
+        return config;
+    },
 };
 
 export default nextConfig;
